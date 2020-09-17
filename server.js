82eb507435d0bb5f777271ca85deaffde5d7b60e
akjha96 <akjha96@gmail.com>
2020-09-17 14:09:23 +0530

    Final backend upload

---
+++ server.js
@@ -0,0 +1,28 @@
+// import dependencies
+import express from "express";
+import cors from "cors";
+
+// app config
+const app = express();
+const port = 3001;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// api routes
+app.get("/", (req, res) => {
+  res.status(200).send("hello World");
+});
+
+app.post("/api/v1/calculateTip", (req, res) => {
+  const amount = parseInt(req.body.amount);
+  const percentage = parseInt(req.body.percentage);
+
+  const toBePayed = (percentage / 100) * amount;
+
+  res.status(200).json({ toBePayed });
+});
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
